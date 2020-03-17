4d8816de1ad687d3e1fd2e012ffa0f3336826793
alexkozlov0709 <59420903+alexkozlov0709@users.noreply.github.com>
2020-03-17 23:54:09 +0200

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,55 @@
+ublic class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) {
+            this.age = 0;
+        } else if (age > 0){
+            this.age = age;
+        }
+    }
+
+    public boolean isTeen() {
+        if (age > 12 && age < 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            System.out.println();
+        } else if (lastName.isEmpty()) {
+            return firstName;
+        } else if (firstName.isEmpty()) {
+            return lastName;
+        } else {
+            return this.firstName + " " + this.lastName;
+        }
+        return this.firstName + " " + this.lastName;
+    }
+}
